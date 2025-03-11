a982d91160ec1f4db0fa75de4a1623729d38dc5e
YuYeaJin <yyj9290@naver.com>
2025-03-11 18:11:51 +0900

    [예진] 강의 페이지 상단 부 설정

--- lib/pages/introducePage.dart
+++ lib/pages/introducePage.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
 class IntroducePage extends StatelessWidget {
-  const IntroducePage({super.key});
+  final String category;
+  final String subCategory;
+  final String lectureTitle;
+
+  const IntroducePage({
+    super.key,
+    this.category = "실내",
+    this.subCategory = "요가",
+    this.lectureTitle = "기초 요가 스트레칭",
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '$category / $subCategory', // 대분류 / 소분류
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.white70,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              lectureTitle, // 강의 제목
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.blue,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: const Center(child: Text('강의 영상이 들어갈 자리')),
+    );
   }
 }
