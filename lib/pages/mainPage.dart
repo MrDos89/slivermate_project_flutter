3382079da72207efade91902484bdb1b7311ea6e
김도형 <mrdos@naver.com>
2025-03-11 16:22:50 +0900

    [도형] 임시로 각 화면으로 가는 버튼 생성

--- lib/pages/mainPage.dart
+++ lib/pages/mainPage.dart
@@ -5,6 +5,57 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("메인페이지"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Container(color: Colors.grey[100], child: _MainPage()),
+    );
+  }
+}
+
+class _MainPage extends StatefulWidget {
+  const _MainPage({super.key});
+
+  @override
+  State<_MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<_MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/category");
+                },
+                child: Text("카데고리 화면으로 이동"),
+              ),
+            ),
+            SizedBox(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/introduce");
+                },
+                child: Text("인트로 화면으로 이동"),
+              ),
+            ),
+            SizedBox(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/purchase");
+                },
+                child: Text("결제 화면으로 이동"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
