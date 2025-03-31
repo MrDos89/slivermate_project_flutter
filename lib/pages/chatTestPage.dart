8a03a1ebc25937bb54130485f1268a43053e92fd
junil3005 <yjl3005@naver.com>
2025-03-31 11:58:14 +0900

    [준일] 채팅창화면 ui구현

--- lib/pages/chatTestPage.dart
+++ lib/pages/chatTestPage.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:slivermate_project_flutter/components/mainLayout.dart';
 import 'package:slivermate_project_flutter/components/headerPage.dart';
-
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ChatTestPage extends StatelessWidget {
@@ -33,22 +32,19 @@ class _ChatTestPage extends StatefulWidget {
 class _ChatTestPageState extends State<_ChatTestPage> {
   final TextEditingController _controller = TextEditingController();
   late WebSocketChannel _channel;
-  final List<Map<String, String>> _messages = []; // 메시지를 보낸 사람(sender) 정보 포함
+  final List<Map<String, String>> _messages = [];
 
   @override
   void initState() {
     super.initState();
-
     String slivermateChat = dotenv.get("SLIVERMATECHAT");
 
-    // WebSocket 연결
     _channel = WebSocketChannel.connect(
       Uri.parse(
         "wss://$slivermateChat.execute-api.ap-northeast-2.amazonaws.com/production/",
       ),
     );
 
-    // 서버에서 메시지 받기
     _channel.stream.listen((message) {
       setState(() {
         _messages.add({'sender': 'server', 'message': message});
@@ -56,7 +52,6 @@ class _ChatTestPageState extends State<_ChatTestPage> {
     });
   }
 
-  // 메시지 보내기
   void _sendMessage() {
     if (_controller.text.isNotEmpty) {
       final message = json.encode({
@@ -67,19 +62,20 @@ class _ChatTestPageState extends State<_ChatTestPage> {
       setState(() {
         _messages.add({'sender': 'me', 'message': _controller.text});
       });
-      _controller.clear(); // 메시지 입력란 비우기
+      _controller.clear();
     }
   }
 
   @override
   void dispose() {
-    _channel.sink.close(); // WebSocket 종료
+    _channel.sink.close();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white70,
       appBar: AppBar(title: Text('채팅 테스트 중')),
       body: Column(
         children: [
@@ -89,25 +85,139 @@ class _ChatTestPageState extends State<_ChatTestPage> {
               itemBuilder: (context, index) {
                 final message = _messages[index];
                 final isMe = message['sender'] == 'me';
+                final senderName = isMe ? "파릇" : "게스트";
+                final senderImage =
+                    isMe
+                        ? NetworkImage(
+                          "https://cdn-icons-png.flaticon.com/512/147/147144.png",
+                        )
+                        : NetworkImage(
+                          "https://cdn-icons-png.flaticon.com/512/194/194938.png",
+                        );
 
-                return ListTile(
-                  title: Align(
-                    alignment:
-                        isMe ? Alignment.centerRight : Alignment.centerLeft,
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: isMe ? Colors.blue : Colors.grey[300],
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Text(
-                        message['message']!,
-                        style: TextStyle(
-                          color: isMe ? Colors.white : Colors.black,
+                final messageTime = DateTime.now();
+                final timeString =
+                    "${messageTime.hour.toString().padLeft(2, '0')}:${messageTime.minute.toString().padLeft(2, '0')}";
+
+                bool showDateHeader = false;
+                if (index == 0) {
+                  showDateHeader = true;
+                } else {
+                  final prevDate = DateTime.now().subtract(Duration(days: 1));
+                  if (messageTime.day != prevDate.day ||
+                      messageTime.month != prevDate.month ||
+                      messageTime.year != prevDate.year) {
+                    showDateHeader = true;
+                  }
+                }
+
+                return Column(
+                  crossAxisAlignment:
+                      isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                  children: [
+                    if (showDateHeader)
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Center(
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 5,
+                              horizontal: 12,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.grey[800],
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Text(
+                              "${messageTime.year}년 ${messageTime.month}월 ${messageTime.day}일 (${["일", "월", "화", "수", "목", "금", "토"][messageTime.weekday % 7]})",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8,
+                        vertical: 4,
+                      ),
+                      child: Row(
+                        mainAxisAlignment:
+                            isMe
+                                ? MainAxisAlignment.end
+                                : MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (!isMe) ...[
+                            CircleAvatar(
+                              backgroundImage: senderImage,
+                              radius: 18,
+                            ),
+                            SizedBox(width: 8),
+                          ],
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment:
+                                  isMe
+                                      ? CrossAxisAlignment.end
+                                      : CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  senderName,
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.grey[400],
+                                  ),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.only(top: 4),
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    color:
+                                        isMe
+                                            ? Colors.yellow[700]
+                                            : Colors.grey[800],
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(12),
+                                      topRight: Radius.circular(12),
+                                      bottomLeft:
+                                          isMe
+                                              ? Radius.circular(12)
+                                              : Radius.zero,
+                                      bottomRight:
+                                          isMe
+                                              ? Radius.zero
+                                              : Radius.circular(12),
+                                    ),
+                                  ),
+                                  child: Text(
+                                    message['message'] ?? '',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                      height: 1.4,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 2),
+                                Text(
+                                  timeString,
+                                  style: TextStyle(
+                                    color: Colors.grey[500],
+                                    fontSize: 10,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
+                  ],
                 );
               },
             ),
@@ -116,13 +226,71 @@ class _ChatTestPageState extends State<_ChatTestPage> {
             padding: const EdgeInsets.all(8.0),
             child: Row(
               children: [
+                IconButton(
+                  icon: Icon(Icons.add, color: Colors.white),
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return Container(
+                          height: 150,
+                          color: Colors.white,
+                          child: Column(
+                            children: [
+                              ListTile(
+                                leading: Icon(Icons.camera_alt),
+                                title: Text("카메라로 촬영하기"),
+                                onTap: () {
+                                  Navigator.pop(context);
+                                  // TODO: 카메라 기능 추가
+                                },
+                              ),
+                              ListTile(
+                                leading: Icon(Icons.photo_library),
+                                title: Text("앨범에서 선택하기"),
+                                onTap: () {
+                                  Navigator.pop(context);
+                                  // TODO: 앨범 기능 추가
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
                 Expanded(
                   child: TextField(
                     controller: _controller,
-                    decoration: InputDecoration(hintText: 'Enter your message'),
+                    onChanged: (_) => setState(() {}),
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: '메시지를 입력하세요',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      filled: true,
+                      fillColor: Colors.grey[900],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
                   ),
                 ),
-                IconButton(icon: Icon(Icons.send), onPressed: _sendMessage),
+                IconButton(
+                  icon: Icon(
+                    Icons.emoji_emotions_outlined,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    // TODO: 이모티콘 기능 추가
+                  },
+                ),
+                if (_controller.text.isNotEmpty)
+                  IconButton(
+                    icon: Icon(Icons.send, color: Colors.white),
+                    onPressed: _sendMessage,
+                  ),
               ],
             ),
           ),
