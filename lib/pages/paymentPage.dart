4019b62725ee42c7378efba3a90417e0c12ff73a
junil3005 <yjl3005@naver.com>
2025-04-04 12:29:32 +0900

    [준일] 결제수단 체크반응 추가

--- lib/pages/paymentPage.dart
+++ lib/pages/paymentPage.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:slivermate_project_flutter/components/mainLayout.dart';
 import 'package:slivermate_project_flutter/components/headerPage.dart';
 
-class PaymentPage extends StatelessWidget {
+class PaymentPage extends StatefulWidget {
   const PaymentPage({super.key});
+
+  @override
+  _PaymentPageState createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  int? _selectedPaymentMethod = 0; // 결제 수단의 기본 선택값
 
   @override
   Widget build(BuildContext context) {
@@ -19,13 +26,18 @@ class PaymentPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               // 파릇 정보
+              Text(
+                "파릇 정보",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+
+              const SizedBox(height: 20),
+
               Row(
                 children: [
                   CircleAvatar(
                     radius: 30,
-                    backgroundImage: AssetImage(
-                      'lib/images/당구.jpg',
-                    ), //  썸네일 이미지
+                    backgroundImage: AssetImage('lib/images/당구.jpg'), // 썸네일 이미지
                   ),
                   const SizedBox(width: 12),
                   Expanded(
@@ -39,18 +51,21 @@ class PaymentPage extends StatelessWidget {
                   ),
                 ],
               ),
-              const SizedBox(height: 20),
-
-              //  일정 정보
+              const SizedBox(height: 10),
+              Divider(), // 구분선 추가
+              // 일정 정보
+              const SizedBox(height: 10),
               Text(
                 "일정 정보",
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 8),
               Text("4월 05일 (토) 오후 3:00", style: TextStyle(fontSize: 14)),
-              const SizedBox(height: 20),
+              const SizedBox(height: 10),
 
-              //  결제 수단
+              Divider(), // 구분선 추가
+              // 결제 수단
+              const SizedBox(height: 10),
               Text(
                 "결제수단",
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
@@ -58,35 +73,55 @@ class PaymentPage extends StatelessWidget {
               const SizedBox(height: 8),
               Column(
                 children: [
-                  RadioListTile(
+                  RadioListTile<int>(
                     title: const Text("신용/체크카드"),
                     value: 1,
-                    groupValue: 0,
-                    onChanged: (value) {},
+                    groupValue: _selectedPaymentMethod,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedPaymentMethod = value;
+                      });
+                    },
+                    secondary: Icon(Icons.credit_card),
                   ),
-                  RadioListTile(
+                  RadioListTile<int>(
                     title: const Text("카카오페이"),
                     value: 2,
-                    groupValue: 0,
-                    onChanged: (value) {},
+                    groupValue: _selectedPaymentMethod,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedPaymentMethod = value;
+                      });
+                    },
+                    secondary: Icon(Icons.payment),
                   ),
-                  RadioListTile(
+                  RadioListTile<int>(
                     title: const Text("애플페이"),
-                    value: 4,
-                    groupValue: 0,
-                    onChanged: (value) {},
+                    value: 3,
+                    groupValue: _selectedPaymentMethod,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedPaymentMethod = value;
+                      });
+                    },
+                    secondary: Icon(Icons.apple),
                   ),
-                  RadioListTile(
+                  RadioListTile<int>(
                     title: const Text("휴대폰 결제"),
-                    value: 6,
-                    groupValue: 0,
-                    onChanged: (value) {},
+                    value: 4,
+                    groupValue: _selectedPaymentMethod,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedPaymentMethod = value;
+                      });
+                    },
+                    secondary: Icon(Icons.phone_iphone),
                   ),
                 ],
               ),
               const SizedBox(height: 20),
 
-              //  결제금액, 프립금액, 수수료 표시
+              // 결제금액, 프립금액, 수수료 표시
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -110,7 +145,7 @@ class PaymentPage extends StatelessWidget {
                 children: [
                   GestureDetector(
                     onTap: () {
-                      //  결제하기 버튼 클릭 시 처리 로직
+                      // 결제하기 버튼 클릭 시 처리 로직
                     },
                     child: Container(
                       padding: const EdgeInsets.symmetric(
@@ -128,10 +163,10 @@ class PaymentPage extends StatelessWidget {
                     ),
                   ),
 
-                  //  취소하기 버튼
+                  // 취소하기 버튼
                   GestureDetector(
                     onTap: () {
-                      //  취소하기 버튼 클릭 시 처리 로직
+                      // 취소하기 버튼 클릭 시 처리 로직
                     },
                     child: Container(
                       padding: const EdgeInsets.symmetric(
